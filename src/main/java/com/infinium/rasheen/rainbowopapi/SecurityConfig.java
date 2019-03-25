84fb2a53b3c968ccf839022548e83aa46917daf2
RasheenRuwisha <sheen.ruwisha@gmail.com>
2019-03-25 23:23:32 +0530

    first commit

---
+++ src/main/java/com/infinium/rasheen/rainbowopapi/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.infinium.rasheen.rainbowopapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/**").authenticated()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+
+
+
+}
